7ada9ec7182c55d8d6b2f3da2790512500b9baca
thomas0x2 <tbollenbach@icloud.com>
2024-03-19 23:41:00 +0100

    added dropdown

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 function showDropdown() {
-    var elem = document.getElementById("menuDropdown")
-    elem.classList.toggle("show");
+    var elem = document.getElementById("menuDropdown");
+    if (elem.classList.contains("hidden")) {
+        elem.classList.remove("hidden");
+    }
+    else {
+        elem.classList.add("hidden")
+    }
 }
