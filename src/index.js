a02334df5478fa737d2b5ddf5a24b6f73602b272
thomas0x2 <tbollenbach@icloud.com>
2025-05-07 16:13:06 +0200

    cleaned repo

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function showDropdown() {
-    var elem = document.getElementById("menuDropdown");
-    if (elem.classList.contains("hidden")) {
-        elem.classList.remove("hidden");
-    }
-    else {
-        elem.classList.add("hidden")
-    }
-}
